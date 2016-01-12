3bcce1d468b0fe22f433611170045d6c749266bb
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 18:00:56 -0800

    Got a little bit into Further Study.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,12 +1,14 @@
 import sys
 
+restaurant_and_ratings = {}
+restaurant_filename = sys.argv[1]
 
-def show_restaurant_ratings(filename): 
+def read_restaurant_ratings(filename = restaurant_filename): 
 
     """Shows ratings by restaurant"""
 
     with open(filename) as ratings_file:
-        restaurant_and_ratings = {}
+
         for line in ratings_file:
             line = line.rstrip()
             restaurants = line.split(":")
@@ -14,12 +16,44 @@ def show_restaurant_ratings(filename):
             restaurant_and_ratings[restaurants[0]] = restaurants[1]
 
 
-    alphabetical_restaurants = sorted(restaurant_and_ratings.items())
+    return restaurant_and_ratings
+
+
+def alphabetical_restaurant_ratings(restaurant_ratings_dict): 
+    """Print alphabetically sorted restaurant ratings"""
+    
+
+    alphabetical_restaurants = sorted(read_restaurant_ratings(restaurant_ratings_dict).items())
 
     for restaurant, rating in alphabetical_restaurants:
         print "%s is rated at %s." % (restaurant, rating)
 
 
 
+def get_new_restaurant_entry(restaurant_ratings = restaurant_and_ratings): 
+    """Get raw input for new restaurant and rating"""
 
-show_restaurant_ratings(sys.argv[1])            
+    restaurant_and_ratings = read_restaurant_ratings(restaurant_filename)
+    print restaurant_and_ratings
+
+    restaurant_name = ""
+    # restaurant_name = raw_input("What is the name of the restaurant you are rating? Or type 'Quit' if done. ")
+    # if restaurant_name == "Quit":
+    #     return None
+    while restaurant_name != "Quit": 
+        restaurant_name = raw_input("What is the name of the restaurant you are rating? Or type 'Quit' if done. ")
+        if restaurant_name == "Quit":
+            break
+            #return None
+        rating = int(raw_input("What rating do you give this restaurant? "))
+        print restaurant_name
+        #get_new_restaurant_entry()
+        restaurant_and_ratings[restaurant_name] = rating
+        print restaurant_and_ratings
+
+
+    alphabetical_restaurant_ratings(restaurant_and_ratings)
+
+
+#alphabetical_restaurant_ratings(restaurant_filename) 
+get_new_restaurant_entry()
